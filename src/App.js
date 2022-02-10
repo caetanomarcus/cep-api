caf17f17ca3e9eb0bd40d38eb300c50e6bff4c6a
Marcus <caetanomarcusf@gmail.com>
2022-02-10 19:42:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state ={
+    cep: '',
+    rua: '',
+    bairro: '',
+    cidade: '',
+    estado: ''
+  }
+
+  handleBlur = () => {
+    fetch(`https://ws.apicep.com/cep/${this.state.cep}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+
+      this.setState({
+        rua: data.address,
+        bairro: data.district,
+        cidade: data.city,
+        estado: data.state
+      })
+    })
+  }
+
+  render() {
+      const {cep, rua, bairro, cidade, estado} = this.state
+    return (
+      <div>
+        <h1>{this.state.cep}</h1>
+        <label>
+            cep:
+          <input value={cep} onChange={(event) => this.setState({
+            cep: event.target.value
+          })}
+        onBlur={this.handleBlur}
+          />
+        </label>
+        <label>
+            rua:
+          <input value={rua} readOnly />
+        </label>
+        <label>
+          bairro:
+          <input value={bairro} readOnly />
+        </label>
+        <label>
+          cidade:
+          <input value={cidade} readOnly/>
+        </label>
+        <label>
+          estado:
+          <input value={estado} readOnly/>
+        </label>
+      </div>
+    );
+  }
 }
 
 export default App;
